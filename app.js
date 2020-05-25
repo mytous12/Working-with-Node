5cbd2b1ba09f6de4dd6a42cb411aa3a46b6c5415
Shivam <shivampundir@rocketmail.com>
2020-05-25 19:38:39 +0530

    Using Passport for authentication

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticated = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -40,23 +42,20 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
-  if (!req.session.user) {
+  console.log(req.user);
+  if (!req.user) {
     var err = new Error('You are not authenticated');
-    err.status = 401;
-    return next(err);
+    err.status = 403;
+    next(err);
   } else {
-    if (req.session.user === 'authenticated') {
-      next();
-    } else {
-      var err = new Error('You are not authenticated');
-      err.status = 401;
-      return next(err);
-    }
+    next();
   }
 }
 
